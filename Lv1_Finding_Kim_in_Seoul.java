484d5c6b2cc05928b650a48be0cfeea472634203
KYEONGRYONG OH <96464756+okr4306@users.noreply.github.com>
2021-12-27 00:39:20 +0900

    Create Lv1_Finding_Kim_in_Seoul.java
    문제 설명
    String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요. seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
    
    제한 사항
    seoul은 길이 1 이상, 1000 이하인 배열입니다.
    seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
    "Kim"은 반드시 seoul 안에 포함되어 있습니다.

---
+++ Lv1_Finding_Kim_in_Seoul.java
@@ -0,0 +1,13 @@
+// 서울에서 김서방 찾기
+
+class Solution {
+	public String solution(String[] seoul) {
+		String answer = "";
+		for (int i = 0; i < seoul.length; i++) {
+			if ("Kim".equals(seoul[i])) {
+				answer = "김서방은 " + i + "에 있다";
+			}
+		}
+		return answer;
+	}
+}
